cf02b73993a727c3cfeb38bf9c8094e407a390e7
crinelam <crinelam@aol.com>
2026-04-27 21:24:03 +0200

    Added Tabbed section with placeholders.

--- src/CharGal/main.py
+++ src/CharGal/main.py
@@ -1,6 +1,22 @@
 import sys
 
-from PySide6 import QtWidgets, QMainWindow
+# from PySide6.QtCore import Qt
+from PySide6.QtGui import QColor, QPalette
+from PySide6.QtWidgets import QMainWindow, QTabWidget, QWidget, QApplication
+
+
+class Color(QWidget):
+    """Simple color widget."""
+
+    def __init__(self, color):
+        """Initialize widget."""
+        super().__init__()
+
+        self.setAutoFillBackground(True)
+
+        palette = self.palette()
+        palette.setColor(QPalette.ColorRole.Window, QColor(color))
+        self.setPalette(palette)
 
 
 class MainWindow(QMainWindow):
@@ -12,9 +28,18 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("Character Gallery")
 
+        tabs = QTabWidget()
+        tabs.setTabPosition(QTabWidget.North)
+        tabs.setMovable(True)
+
+        for color in ["red", "green", "blue", "yellow"]:
+            tabs.addTab(Color(color), color)
+
+        self.setCentralWidget(tabs)
+
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
+    app = QApplication(sys.argv)
 
     window = MainWindow()
     window.show()
