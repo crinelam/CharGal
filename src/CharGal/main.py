7ba8abc055836db7c3c49073738f6f3f2f0f4bc1
crinelam <crinelam@aol.com>
2026-05-07 14:47:17 +0200

    Only allow one instance of the program to run.

--- src/CharGal/main.py
+++ src/CharGal/main.py
@@ -15,13 +15,13 @@
 #        along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import sys
-
-# from PySide6.QtCore import Qt
+from tendo import singleton
 from PySide6.QtWidgets import QApplication
 from cgqt.Windows import MainWindow
 
 
 if __name__ == "__main__":
+    me = singleton.SingleInstance()
     app = QApplication(sys.argv)
 
     window = MainWindow()
